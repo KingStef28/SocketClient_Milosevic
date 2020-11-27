059804afdde7e333962ae2e30e9b7923ec9e314c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-27 12:34:58 +0100

    Completamento codice

--- SocketClient_Milosevic/Program.cs
+++ SocketClient_Milosevic/Program.cs
@@ -9,72 +9,94 @@ namespace SocketClient_Milosevic
     {
         static void Main(string[] args)
         {
-            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            
+            // Dichiarazione Socket Client
+            Socket client = null;
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // Dichiarazione EndPoint del server
+            IPAddress ipAddr = null;
             string strIPAddress = "";
             string strPort = "";
-            IPAddress ipAddress = null;
-            int nPort;
+            int nPort = 0;
 
+
+            //Dichiarazione Variabili per comunicare con il server
+            string sendString = "";
+            string receivedString = "";
+            byte[] sendBuff = new byte[128];
+            byte[] recvBuff = new byte[128];
+            int nReceivedBytes = 0;
             try
             {
-                Console.WriteLine("IP del server: ");
+                // Settagio da Console dell'EndPoint
+                Console.WriteLine("Benvenuto nel Client Socket");
+                Console.Write("IP del Server: ");
                 strIPAddress = Console.ReadLine();
-                Console.WriteLine();
-                Console.WriteLine("Porta del server: ");
+                Console.Write("Porta del Server: ");
                 strPort = Console.ReadLine();
 
-                if (!IPAddress.TryParse(strIPAddress.Trim(), out ipAddress))
+                if (!IPAddress.TryParse(strIPAddress.Trim(), out ipAddr))
                 {
-                    Console.WriteLine("IP non valido.");
+                    Console.Write("IP non valido.");
                     return;
                 }
-
                 if (!int.TryParse(strPort, out nPort))
                 {
-                    Console.WriteLine("Porta non valida.");
+                    Console.Write("Porta non valida.");
                     return;
                 }
-
                 if (nPort <= 0 || nPort >= 65535)
                 {
-                    Console.WriteLine("Porta non valida.");
+                    Console.Write("Porta non valida.");
                     return;
                 }
+                Console.WriteLine("Endpoint: " + ipAddr.ToString() + " " + nPort);
 
-                Console.WriteLine();
-                Console.WriteLine("Endpoint del server " + ipAddress.ToString() + " " + nPort);
+                // Connessione al server
+                client.Connect(ipAddr, nPort);
 
-                client.Connect(ipAddress, nPort);
-
-                byte[] buff = new byte[128];
-                string sendString = "";
-                string receiveString = "";
-                int recevivedBytes = 0;
-
+                //Inizio chat con il server
+                Console.WriteLine("Chatta con il server. ");
                 while (true)
                 {
-                    Console.WriteLine("Manda un messaggio: ");
+                    // Prendo il messaggio & condizione di uscita
                     sendString = Console.ReadLine();
-                    buff = Encoding.ASCII.GetBytes(sendString);
-                    client.Send(buff);
+                    //dico al server di interrompersi
+                    sendBuff = Encoding.ASCII.GetBytes(sendString);
+                    client.Send(sendBuff);
+
                     if (sendString.ToUpper().Trim() == "QUIT")
                     {
+
                         break;
                     }
-                    Array.Clear(buff, 0, buff.Length);
-                    recevivedBytes = client.Receive(buff);
-                    receiveString = Encoding.ASCII.GetString(buff, 0, recevivedBytes);
-
-                    Console.WriteLine("S: " + receiveString);
-
-
+                    //Pulisco il buffer e ricevo il messaggio
+                    Array.Clear(recvBuff, 0, recvBuff.Length);
+                    nReceivedBytes = client.Receive(recvBuff);
+                    receivedString = Encoding.ASCII.GetString(recvBuff);
+                    Console.WriteLine("S: " + receivedString);
                 }
+
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+            finally
+            {
+                /* In ogni occasione chiudo la connessione per sicurezza */
+                if (client != null)
+                {
+                    if (client.Connected)
+                    {
+                        client.Shutdown(SocketShutdown.Both);//disabilita la send e receive
+                    }
+                    client.Close();
+                    client.Dispose();
+                }
+            }
+            Console.WriteLine("Premi Enter per chiudere...");
+            Console.ReadLine();
         }
     }
 }
